4351555d7178b68bd47f0ab0374a175df500f5a6
N1waki <oleg.gunya2@gmail.com>
2022-11-23 14:57:24 +0100

    123

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BigDecimal ans = BigDecimal.valueOf(0);
         Scanner reader = new Scanner(System.in);
         System.out.print("\nEnter first numbers: ");
         BigDecimal num1 = reader.nextBigDecimal();
@@ -18,40 +17,16 @@ public class Main {
                 }
                 case "f" -> System.exit(0);
                 case "gr" -> {
-                    System.out.print("\nEnter add or mul: ");
-                    op = reader.next();
-                    int dec = num1.subtract(BigDecimal.valueOf(1)).toString().length();
-                    for (BigDecimal up = BigDecimal.valueOf(0); up.compareTo(num1) < 0; up = up.add(BigDecimal.valueOf(1))) {
-                        for (BigDecimal left = BigDecimal.valueOf(0); left.compareTo(num1) < 0; left = left.add(BigDecimal.valueOf(1))) {
-                            switch (op) {
-                                case "add" -> System.out.format("%0" + dec + "d ", (ans = left.add(up)).remainder(num1).intValue());
-                                case "mul" -> System.out.format("%0" + dec + "d ", (ans = left.multiply(up)).remainder(num1).intValue());
-                                default -> System.exit(0);
-                            }
-                        }
-                        System.out.print("\n");
-                    }
+                    Group(num1);
                     System.out.print("\nEnter first numbers: ");
                     num1 = reader.nextBigDecimal();
                 }
                 default -> {
                     System.out.print("\nEnter second numbers: ");
                     BigDecimal num2 = reader.nextBigDecimal();
-                    switch (op) {
-                        case "+" -> ans = num1.add(num2);
-                        case "-" -> ans = num1.subtract(num2);
-                        case "*" -> ans = num1.multiply(num2);
-                        case "/" -> ans = num1.divide(num2, 100, RoundingMode.HALF_UP);
-                        case "^" -> ans = num1.pow(num2.intValue());
-                        case "mod" -> ans = num1.remainder(num2);
-                        case "log" -> {
-                            ans = new BigDecimal(1);
-                            ans = ans.divide(BigDecimal.valueOf(Math.log(num2.doubleValue()) / Math.log(num1.doubleValue())), 100, RoundingMode.HALF_UP);
-                        }
-                        default -> System.exit(0);
-                    }
-                    System.out.print("\n" + Clear(num1.toString()) + " " + op + " " + Clear(num2.toString()) + " = " + Clear(ans.toString()));
-                    num1 = ans;
+                    BigDecimal result = Calculate(num1, num2, op);
+                    System.out.print("\n" + Clear(num1.toString()) + " " + op + " " + Clear(num2.toString()) + " = " + Clear(result.toString()));
+                    num1 = result;
                 }
             }
         }
@@ -63,21 +38,35 @@ public class Main {
         }
         return noNulls;
     }
-    /**public static BigDecimal Calculation(BigDecimal num1, BigDecimal num2, String op){
-     BigDecimal ans = BigDecimal.valueOf(0);
-     Scanner reader = new Scanner(System.in);
-     switch (op) {
-     case "+" -> ans = num1.add(num2);
-     case "-" -> ans = num1.subtract(num2);
-     case "*" -> ans = num1.multiply(num2);
-     case "/" -> ans = num1.divide(num2, 100, RoundingMode.HALF_UP);
-     case "^" -> ans = num1.pow(num2.intValue());
-     case "mod" -> ans = num1.remainder(num2);
-     case "log" -> {
-     ans = new BigDecimal(1);
-     ans = ans.divide(BigDecimal.valueOf(Math.log(num2.doubleValue()) / Math.log(num1.doubleValue())), 100, RoundingMode.HALF_UP);
+    public static void Group(BigDecimal num){
+        System.out.print("\nEnter add or mul: ");
+        String op = new Scanner(System.in).next();
+        int numint = num.intValue();
+        int dec = String.valueOf(numint - 1).length();
+        for (int up = 0; up < numint; up++) {
+            for (int left = 0; left < numint; left++) {
+                switch (op) {
+                    case "add" -> System.out.format("%0" + dec + "d ", (left + up) % numint);
+                    case "mul" -> System.out.format("%0" + dec + "d ", (left * up) % numint);
+                    default -> System.exit(0);
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+    public static BigDecimal Calculate(BigDecimal num1, BigDecimal num2, String op){
+        BigDecimal ans = BigDecimal.valueOf(1);
+        switch (op) {
+            case "+" -> ans = num1.add(num2);
+            case "-" -> ans = num1.subtract(num2);
+            case "*" -> ans = num1.multiply(num2);
+            case "/" -> ans = num1.divide(num2, 100, RoundingMode.HALF_UP);
+            case "^" -> ans = num1.pow(num2.intValue());
+            case "mod" -> ans = num1.remainder(num2);
+            case "log" -> ans = ans.divide(BigDecimal.valueOf(Math.log(num2.doubleValue()) / Math.log(num1.doubleValue())), 100, RoundingMode.HALF_UP);
+            default -> System.exit(0);
+        }
+        return ans;
      }
-     }
-     return ans;
-     }*/
+
 }
