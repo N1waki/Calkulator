17677bf262034f23ddbedeadf078fb2b50253900
N1waki <oleg.gunya2@gmail.com>
2023-09-28 17:01:54 +0200

    Calkulator

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,13 @@ import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
-    static class InvalidInputException extends Exception {
-
+    public static boolean scanDouble(String input){
+        for (int i = 0; i < input.length(); i++)
+            if ((input.charAt(i) < '0' || input.charAt(i) > '9') && input.charAt(i) != '.')
+                return false;
+        return true;
     }
-    public static BigDecimal scanDouble(String input) throws InvalidInputException {
-        for (int i = 0; i < input.length(); i++) {
-            if ((input.charAt(i) < '0' || input.charAt(i) > '9') && input.charAt(i) != '.') {
-                throw new InvalidInputException();
-            }
-        }
-        return BigDecimal.valueOf(Double.parseDouble(input));
-    }
-        public static BigDecimal Calculate(BigDecimal num1, BigDecimal num2, String op){
+    public static BigDecimal Calculate(BigDecimal num1, BigDecimal num2, String op){
         BigDecimal ans = BigDecimal.valueOf(0);
         switch (op) {
             case "+" -> ans = num1.add(num2);
@@ -27,18 +22,16 @@ public class Main {
         }
         return ans;
     }
-    public static void Group(int num){
-        System.out.print("\nEnter add or mul: ");
-        String op = new Scanner(System.in).next();
+    public static void Group(int num, String op){
         int dec = String.valueOf(num - 1).length();
-        for (int up = 0; up < num; up++) {
-            for (int left = 0; left < num; left++) {
-                switch (op) {
-                    case "add" -> System.out.format("%0" + dec + "d ", (left + up) % num);
-                    case "mul" -> System.out.format("%0" + dec + "d ", (left * up) % num);
+            for (int up = 0; up < num; up++) {
+                for (int left = 0; left < num; left++) {
+                    switch (op) {
+                        case "add" -> System.out.format("%0" + dec + "d ", (left + up) % num);
+                        case "mul" -> System.out.format("%0" + dec + "d ", (left * up) % num);
+                    }
                 }
-            }
-            System.out.print("\n");
+                System.out.print("\n");
         }
     }
     public static String Clear(String noNulls) {
@@ -48,46 +41,52 @@ public class Main {
         }
         return noNulls;
     }
-    public static void main(String[] args) throws InvalidInputException {
-        Scanner reader = new Scanner(System.in);
-        boolean ifInputIsInvalid = true;
-        System.out.print("\nEnter first numbers: ");
-        String input = reader.next();
-        while (ifInputIsInvalid) {
-            try {
-                scanDouble(input);
-                ifInputIsInvalid = false;
-            } catch (InvalidInputException e) {
-                System.out.println("Input Error");
-                System.exit(0);
-            } catch (Exception e) {
-                e.printStackTrace();
+    public static BigDecimal getInputNumber(String message, Scanner scanner){
+        boolean ifInputIsValid = true;
+        String input ="";
+        while (ifInputIsValid) {
+            System.out.print(message);
+            input = scanner.next();
+            ifInputIsValid = !scanDouble(input);
+            if (ifInputIsValid) {
+                System.out.println("You're fucking peace of shit");
             }
         }
-        BigDecimal num1 = scanDouble(input);
-        while (true) {
+        return BigDecimal.valueOf(Double.parseDouble(input));
+    }
+    public static void main(String[] args) {
+        boolean finish = false;
+        Scanner reader = new Scanner(System.in);
+        BigDecimal num1 = getInputNumber("Enter first number: ", reader);
+        while (!finish) {
             System.out.print("\nEnter an operator (+, -, *, /, ^, mod, log, f(finish), n(new), gr(groups): ");
             String op = reader.next();
             switch (op) {
                 case "n" -> {
-                    System.out.print("\nEnter first numbers: ");
-                    input = reader.next();
-                    num1 = scanDouble(input);
+                    num1 = getInputNumber("Enter first number: ", reader);
                 }
-                case "f" -> System.exit(0);
+                case "f" -> {
+                    finish = true;
+                    reader.close();
+                }
                 case "gr" -> {
-                    Group(num1.intValue());
-                    System.out.print("\nEnter first numbers: ");
-                    input = reader.next();
-                    num1 = scanDouble(input);
+                    boolean check = true;
+                    while (check) {
+                        System.out.print("\nEnter add or mul: ");
+                        op = reader.next();
+                        if (op.equals("add") || op.equals("mul")) {
+                            Group(num1.intValue(), op);
+                            check = false;
+                        } else {
+                            System.out.println("Syntax.error");
+                        }
+                    }
                 }
                 case "+", "-", "*", "/", "^", "mod", "log" -> {
-                        System.out.print("\nEnter second numbers: ");
-                        input = reader.next();
-                        BigDecimal num2 = scanDouble(input);
-                        BigDecimal result = Calculate(num1, num2, op);
-                        System.out.print("\n" + Clear(num1.toString()) + " " + op + " " + Clear(num2.toString()) + " = " + Clear(result.toString()));
-                        num1 = result;
+                    BigDecimal num2 = getInputNumber("Enter second number: ", reader);
+                    BigDecimal result = Calculate(num1, num2, op);
+                    System.out.print("\n" + Clear(num1.toString()) + " " + op + " " + Clear(num2.toString()) + " = " + Clear(result.toString()));
+                    num1 = result;
                 }
                 default -> System.out.println("Syntax.Error");
             }
